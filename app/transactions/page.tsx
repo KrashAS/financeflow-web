9214ff0b937963a6a42cda0a05216f964fa661a1
Andrei <askrashennikov@gmail.com>
2025-05-13 21:02:15 +0300

    added model transaction

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,33 +1,27 @@
 import WrapperForPage from "@/components/layout/WrapperForPage";
 import UnauthorizedMessage from "@/components/pages/auth/UnauthorizedMessage";
-import TransactionsList from "@/components/pages/transactions/TransactionsList";
+import TransactionsPageContent from "@/components/pages/transactions/TransactionsPageContent";
+import { prisma } from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/authOptions";
-
-
-async function getTransactions() {
-    const res = await fetch("http://localhost:3000/api/statistics?userId=42", {
-        next: { revalidate: 0 },
-    });
-
-    if (!res.ok) {
-        throw new Error("Failed to fetch transactions");
-    }
-
-    return res.json();
-}
 
 export default async function TransactionsPage() {
     const session = await getServerSession(authOptions);
     if (!session?.user) return <UnauthorizedMessage />;
-    const transactions = await getTransactions();
+    const userId = session.user.uid;
+
+    const transactions = await prisma.transaction.findMany({
+        where: { userId },
+        orderBy: { createdAt: "desc" },
+        include: { category: true },
+    });
 
     return (
         <WrapperForPage>
-            <div className="p-6 space-y-6">
-                <h1 className="text-2xl font-bold mb-4">Transactions</h1>
-                <TransactionsList transactions={transactions} />
-            </div >
+            <div className="p-6 max-w-6xl mx-auto space-y-6">
+                <h1 className="text-2xl font-bold">📄 Transactions</h1>
+                <TransactionsPageContent transactions={transactions} />
+            </div>
         </WrapperForPage>
     );
 }
